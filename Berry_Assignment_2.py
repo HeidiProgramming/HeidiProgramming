4d79c039fd810806144796f182b148eb94fba0b7
HeidiProgramming <167151978+HeidiProgramming@users.noreply.github.com>
2024-04-15 22:18:04 -0400

    Add files via upload

---
+++ Berry_Assignment_2.py
@@ -0,0 +1,48 @@
+#Variables
+first_name = "heidi"
+last_name = "BERRY"
+
+#print name
+print(f"Hello, {first_name} {last_name}")
+print()
+print()
+
+#variable for full name
+full_name = first_name + " " + last_name
+
+#slice last name 
+print(full_name[6:11])
+
+#replace last naeme 
+full_name = first_name + " " + last_name + ", Walsh College student"
+print(full_name)
+
+#Print out statement 
+print('"Start by doing what\'s necessary; then do what\'s possible; and suddenly you are doing the impossible - Francis of Assisi')
+
+#Store 2 numbers 
+numberOne = 1.0
+numberTwo = 2.0 
+
+#add, sub, multiply, divide
+additionVar = numberOne + numberTwo
+subtractionVar = numberOne - numberTwo
+muliplyVar = numberOne * numberTwo
+divideVar = numberOne / numberTwo
+
+#print values
+print(f"{numberOne} plus {numberTwo} equals {additionVar}")
+print(f"{numberOne} minus {numberTwo} equals {subtractionVar}")
+print(f"{numberOne} multiplied {numberTwo} equals {muliplyVar}")
+print(f"{numberOne} divide {numberTwo} equals {divideVar}")
+
+#square root variable 
+sq_root = muliplyVar**.5
+print(f"The square root of {muliplyVar} equals {sq_root}") 
+
+#Date variables
+currentMonth = "April"
+currentday = 15 
+print(f"\n\t\tToday is day {currentday} of the month of {currentMonth}")
+
+
